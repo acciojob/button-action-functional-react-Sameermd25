b4d06a58dc59b39fac93aff532a9b97868a27da3
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-10-24 14:40:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [displayP,setDisplayP]=useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+        <button id="click" className='border w-[150px] bg-gray-300 text-lg p-2' onClick={()=>setDisplayP(!displayP)}>
+            click to{displayP?<span> hide</span>:<span> show</span>} p
+        </button>
+        {displayP ? <p id="para" className='text-green-600 font-bold text-3xl m-3'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:<p></p>}
+        
     </div>
   );
 }
