a2a31afbd161571f32e2efa128fcd36375d0ae29
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-10-24 14:44:25 +0530

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
         <button id="click" className='border w-[150px] bg-gray-300 text-lg p-2' onClick={()=>setDisplayP(!displayP)}>
             click to{displayP?<span> hide</span>:<span> show</span>} p
         </button>
-        {displayP ? <p id="para" className='text-green-600 font-bold text-3xl m-3'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:<p></p>}
+        {displayP && <p id="para" className='text-green-600 font-bold text-3xl m-3'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
         
     </div>
   );
